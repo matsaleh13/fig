46f6e349792637bf66e0b4691eb2e4557b33fa23
Matthew Walker <matsaleh13@gmail.com>
2018-09-18 23:40:15 -0500

    Set compiler warning options.

--- src/macros.cmake
+++ src/macros.cmake
@@ -10,3 +10,33 @@ macro(debug_message msg)
     set(DBG_LABEL "DBG (${PROJECT_NAME})")
     message ("${DBG_LABEL}: ${msg}")
 endmacro(debug_message)
+
+macro(warnings_strict)
+    if (MSVC)
+        # warning level 4 and all warnings as errors
+        add_compile_options(/W4 /WX)
+    else()
+        # lots of warnings and all warnings as errors
+        add_compile_options(-Wall -Wextra -pedantic -Werror)
+    endif()
+endmacro(warnings_strict)
+
+macro(warnings_moderate)
+    if (MSVC)
+        # warning level 3 and all warnings as errors
+        add_compile_options(/W3 /WX)
+    else()
+        # major warnings and all warnings as errors
+        add_compile_options(-W3 -Werror)
+    endif()
+endmacro(warnings_moderate)
+
+macro(warnings_relaxed)
+    if (MSVC)
+        # warning level 3 and no warnings as errors
+        add_compile_options(/W3)
+    else()
+        # major warnings and no warnings as errors
+        add_compile_options(-W3)
+    endif()
+endmacro(warnings_relaxed)

544996e5a1261956d3a61b86ae84ecb7e393ce98
Matthew Walker <matsaleh13@gmail.com>
2018-09-18 20:23:53 -0500

    Refactor CMake debug macros.

--- src/macros.cmake
+++ src/macros.cmake
@@ -1,7 +1,12 @@
-macro(debug_message)
+
+macro(debug_boilerplate)
+    debug_message("CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
+    debug_message("CMAKE_BINARY_DIR: ${CMAKE_BINARY_DIR}")
+    debug_message("PROJECT_SOURCE_DIR: ${PROJECT_SOURCE_DIR}")
+    debug_message("PROJECT_BINARY_DIR: ${PROJECT_BINARY_DIR}")
+endmacro(debug_boilerplate)
+
+macro(debug_message msg)
     set(DBG_LABEL "DBG (${PROJECT_NAME})")
-    message ("${DBG_LABEL} CMAKE_SOURCE_DIR: ${CMAKE_SOURCE_DIR}")
-    message ("${DBG_LABEL} CMAKE_BINARY_DIR: ${CMAKE_BINARY_DIR}")
-    message ("${DBG_LABEL} PROJECT_SOURCE_DIR: ${PROJECT_SOURCE_DIR}")
-    message ("${DBG_LABEL} PROJECT_BINARY_DIR: ${PROJECT_BINARY_DIR}")
+    message ("${DBG_LABEL}: ${msg}")
 endmacro(debug_message)

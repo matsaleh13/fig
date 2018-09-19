a4c963edaad8978404a5a55bc4b7d371ddcfdc98
Matthew Walker <matsaleh13@gmail.com>
2018-09-18 21:41:11 -0500

    More tests of Result

--- src/fig-util/result.h
+++ src/fig-util/result.h
@@ -6,25 +6,42 @@ namespace util {
 template <typename T, typename E>
 class Result {
 public:
-  Result(const T& value) : m_value(value) {}
-  Result(const E& error) : m_error(error) {}
+  Result(const T& value);
+  Result(const E& error);
+  explicit operator bool() const;
 
   const T& value() const;
   const E& error() const;
 
 private:
+  union {
     T m_value;
     E m_error;
+  } m_data;
+
+  bool m_isError = false;
 };
 
 template <typename T, typename E>
+Result<T, E>::Result(const T& value) : m_data{value}, m_isError(false) {}
+
+template <typename T, typename E>
+Result<T, E>::Result(const E& error) : m_data{error}, m_isError(true) {}
+
+template <typename T, typename E>
+inline Result<T,E>::operator bool() const
+{
+  return (m_isError == false);
+}
+
+template <typename T, typename E>
 inline const T& Result<T, E>::value() const {
-  return m_value;
+  return m_data.m_value;
 }
 
 template <typename T, typename E>
 inline const E& Result<T, E>::error() const {
-  return m_error;
+  return m_data.m_error;
 }
 
 } // namespace util
--- src/test/fig-util-test/fig-util-test.cpp
+++ src/test/fig-util-test/fig-util-test.cpp
@@ -14,3 +14,21 @@ TEST(result_class, create_result_with_bool_error)
   fig::util::Result<int, bool> r(false);
   EXPECT_EQ(r.error(), false);
 }
+
+TEST(result_class, create_result_with_int_value_tests_true)
+{
+  fig::util::Result<int, bool> r(100);
+  EXPECT_TRUE(r);
+}
+
+TEST(result_class, create_result_with_bool_error_tests_false)
+{
+  fig::util::Result<int, bool> r(false);
+  EXPECT_FALSE(r);
+}
+
+// TEST(result_class, create_result_with_int_value_get_error)
+// {
+//   fig::util::Result<int, bool> r(100);
+//   EXPECT_TRUE(r.error());
+// }
